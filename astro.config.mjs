1f9124589b2737b306a529b646cd743de020c93f
Rahit Ash <hi@rashy.me>
2023-10-22 01:57:15 +0530

    Created a basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+console.log(env);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      componentsDir: "src/components",
+    }),
+  ],
 });
