133e7eed19604fca342c6d47cab139f1115dc10c
Rahit Ash <hi@rashy.me>
2023-10-23 00:46:34 +0530

    Changed adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,19 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
-import nodejs from "@astrojs/node";
-
+import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
-
 console.log(env);
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: nodejs({
-    mode: "standalone",
-  }),
-  integrations: [
-    tailwind(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        Article: "components/storyblok/Article",
-      },
-    }),
-  ],
+  integrations: [tailwind(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      Article: "components/storyblok/Article"
+    }
+  })],
+  adapter: vercel()
 });
